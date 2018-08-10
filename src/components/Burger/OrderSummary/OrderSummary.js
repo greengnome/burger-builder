bcf9a46fc697758b377d1c168a183f8215bcc332
Kirill Gladkov <crosfire2011@gmail.com>
2018-08-10 08:26:15 +0300

    added burger builder controls, order btn and modal

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Aux from './../../../hoc/Auxilary';
+import Button from './../../UI/Button/Button';
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map((igKey, i) => {
+            return (
+                <li key={igKey + i}>
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:
+                {props.ingredients[igKey]}
+                </li>
+            )
+        });
+
+    return (
+        <Aux>
+            <h3>Your order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to Checkout?</p>
+            <Button btnType="Danger">CANCEL</Button>
+            <Button btnType="Success">CONTINUE</Button>
+        </Aux>
+    );
+};
+
+export default orderSummary;
