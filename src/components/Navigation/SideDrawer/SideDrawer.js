81d92bb6f2d3727ed12dd2efa1b1ccf70f674468
Kirill Gladkov <KirillG@radware.com>
2018-08-12 10:01:59 +0300

    fixed file names

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import classes from './SideDrawer.css';
+import classes from './SideDrawer.css';
 import Logo from './../../Logo/Logo';
 import NavigationItems from './../NavigationItems/NavigationItems';
 
